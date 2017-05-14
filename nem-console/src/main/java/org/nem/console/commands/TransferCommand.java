c09536c1c2b5737dbb2cde4b6dec03e00f66cf87
Jaguar0625 <jaguar0625@127.0.0.1>
2017-05-14 11:52:20 -0400

    add message option to transfer command

--- nem-console/src/main/java/org/nem/console/commands/TransferCommand.java
+++ nem-console/src/main/java/org/nem/console/commands/TransferCommand.java
@@ -1,9 +1,11 @@
 package org.nem.console.commands;
 
 import org.apache.commons.cli.*;
+import org.nem.core.messages.PlainMessage;
 import org.nem.core.model.*;
 import org.nem.core.model.primitive.Amount;
 import org.nem.core.time.TimeInstant;
+import org.nem.core.utils.StringEncoder;
 
 import java.util.function.Function;
 
@@ -28,17 +30,25 @@ public class TransferCommand extends TransactionCommand {
 		final Account recipient = accountLookup.apply(commandLine.getOptionValue("recipient"));
 		final long amount = Long.parseLong(commandLine.getOptionValue("amount"));
 		System.out.println(String.format("transfer (%s) '%s' -> '%s'", amount, sender, recipient));
+
+		TransferTransactionAttachment attachment = null;
+		if (commandLine.hasOption("message")) {
+			final Message message = new PlainMessage(StringEncoder.getBytes(commandLine.getOptionValue("message")));
+			attachment = new TransferTransactionAttachment(message);
+		}
+
 		return new TransferTransaction(
 				timeStamp,
 				sender,
 				recipient,
 				Amount.fromMicroNem(amount),
-				null);
+				attachment);
 	}
 
 	@Override
 	protected void addCustomOptions(final Options options) {
 		options.addOption("recipient", true, "The recipient alias");
 		options.addOption("amount", true, "The amount in micronem");
+		options.addOption("message", true, "The optional message");
 	}
 }
